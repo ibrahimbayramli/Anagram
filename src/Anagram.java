5a47e87d05e063962792adaa7870cf3a330ab64d
ibrahimbayramli <54009509+ibrahimbayramli@users.noreply.github.com>
2022-11-14 17:50:47 +0300

    bug fixed

--- src/Anagram.java
+++ src/Anagram.java
@@ -6,8 +6,14 @@ public class Anagram {
     public static boolean isAnagram(String firstWord,String secondWord){
         List<String> firsWordList=new ArrayList<>(Arrays.asList(firstWord.toLowerCase().split("")));
         List<String> secondWordList=new ArrayList<>(Arrays.asList(secondWord.toLowerCase().split("")));
-        Set<String> myMap=new HashSet<>(firsWordList);
-        for(String s:myMap){
+        Set<String> firsWordMap=new HashSet<>(firsWordList);
+        Set<String> secondWordMap=new HashSet<>(secondWordList);
+        for(String f:firsWordMap){
+            if(Collections.frequency(firsWordList,f.toLowerCase())!= Collections.frequency(secondWordList,f.toLowerCase())){
+                return false;
+            }
+        }
+        for(String s:secondWordMap){
             if(Collections.frequency(firsWordList,s.toLowerCase())!= Collections.frequency(secondWordList,s.toLowerCase())){
                 return false;
             }
